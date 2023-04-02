2131e1fa9774b09cda0ecf7e39d595995325aa8d
Muhammad Azka <hello.mazka@gmail.com>
2023-04-02 23:26:05 +0700

    still in development

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
+import Layout from '@/components/layout'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+      <Analytics />
+    </Layout>
+  );
 }
