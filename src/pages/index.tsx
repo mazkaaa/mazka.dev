4d7e8cded4b20546c9f567959b24df94508d6be7
Muhammad Azka <hello.mazka@gmail.com>
2023-04-17 00:52:07 +0700

    trying to fix for safari browser

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <div className="absolute mix-blend-difference text-gray-300 w-screen h-screen tracking-wide">
+      <div className="absolute mix-blend-difference text-gray-300 inset-0 tracking-wide">
         <div className="p-4 w-full h-full flex flex-col justify-between">
           <div className="flex flex-row justify-between">
             <Link href={"https://blog.mazka.dev/"} className="">Blog</Link>
@@ -14,8 +14,8 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="container mx-auto">
-        <div className="h-screen">
+      <div className="container mx-auto absolute inset-0">
+        <div className="h-full">
           <div className="flex flex-col z-50 h-full w-full justify-center items-center text-center space-y-2">
             <h3 className="mix-blend-difference text-4xl font-normal text-white">
               Muhammad Azka
